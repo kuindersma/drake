b6f59a7045a60071a6b0320500901e801f8750a5
Zac Manchester <kicksat@gmail.com>
2016-07-22 12:10:31 -0400

    Some tweaks to duplication matrix function

--- drake/util/dupl.m
+++ drake/util/dupl.m
@@ -2,13 +2,10 @@ function D = dupl(n)
 %DUPL Returns the duplication matrix D_n such that vec(S) = D_n*vech(S).
 %This is useful for taking derivatives with respect to symmetric matrices.
 
-a = tril(ones(n));
-i = find(a);
-a(i) = 1:length(i);
-a = a + tril(a,-1)';
-j = vec(a);
-
-D = sparse(1:(n*n), j, ones(n*n,1), n*n, n*(n+1)/2);
+A = tril(ones(n));
+A(logical(A)) = 1:(n*(n+1)/2);
+A = A + tril(A,-1)';
+D = sparse(1:(n*n), A(:), ones(n*n,1), n*n, n*(n+1)/2);
 
 end
 
